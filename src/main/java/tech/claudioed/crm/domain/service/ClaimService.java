b8f7c43c91b80cda1ec527bcd82f5605a642cb8e
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-03-30 12:53:02 -0300

    mapper

--- src/main/java/tech/claudioed/crm/domain/service/ClaimService.java
+++ src/main/java/tech/claudioed/crm/domain/service/ClaimService.java
@@ -1,5 +1,6 @@
 package tech.claudioed.crm.domain.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nats.client.Connection;
 import java.time.Duration;
 import lombok.SneakyThrows;
@@ -19,15 +20,19 @@ public class ClaimService {
 
   private final Connection natsConnection;
 
-  public ClaimService(@Qualifier("natsConnection") Connection natsConnection) {
+  private final ObjectMapper mapper;
+
+  public ClaimService(@Qualifier("natsConnection") Connection natsConnection,
+      ObjectMapper mapper) {
     this.natsConnection = natsConnection;
+    this.mapper = mapper;
   }
 
   @SneakyThrows
   public void notifyClaim(String orderId,Event event){
     log.info("Notifying event {}", event.toString());
     final ClaimRequest claim = ClaimRequest.builder().type("claim").orderId(orderId).data(event.getData()).build();
-    this.natsConnection.publish("request-claims",claim.toString().getBytes());
+    this.natsConnection.publish("request-claims",this.mapper.writeValueAsString(claim).getBytes());
     natsConnection.flush(Duration.ofSeconds(5));
     log.info("Event {} notified successfully", event.getId());
   }
