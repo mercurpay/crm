1215b14cd6ed364d9ff62f020bb8a3ca45a98172
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-03-30 11:57:04 -0300

    config nats

--- src/main/java/tech/claudioed/crm/infra/message/NatsProducer.java
+++ src/main/java/tech/claudioed/crm/infra/message/NatsProducer.java
@@ -3,6 +3,7 @@ package tech.claudioed.crm.infra.message;
 import io.nats.client.Connection;
 import io.nats.client.Nats;
 import io.nats.client.Options;
+import java.time.Duration;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +31,14 @@ public class NatsProducer {
   @Bean("natsConnection")
   @SneakyThrows
   public Connection natsConnection() {
-    return Nats.connect(
-        new Options.Builder().userInfo(natsUser, natsPass).server(natsHost).build());
+    return Nats.connect(new Options.Builder()
+        .connectionTimeout(Duration.ofSeconds(2))
+        .pingInterval(Duration.ofSeconds(10))
+        .reconnectWait(Duration.ofSeconds(1))
+        .userInfo(natsUser,natsPass)
+        .maxReconnects(-1)
+        .reconnectBufferSize(-1)
+        .server(natsHost)
+        .build());
   }
 }
